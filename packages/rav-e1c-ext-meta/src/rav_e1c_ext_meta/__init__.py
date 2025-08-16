2fe8e12935d985efff16897103bed2f080496d72
Anton Rastyazhenko <rastyazhenko.anton@gmail.com>
2025-08-16 06:58:47 +0300

    commit

--- packages/rav-e1c-ext-meta/src/rav_e1c_ext_meta/__init__.py
+++ packages/rav-e1c-ext-meta/src/rav_e1c_ext_meta/__init__.py
@@ -4,24 +4,48 @@ from typing import Protocol, Final, Literal, TypedDict, Type, Annotated
 
 from pydantic import BaseModel, Discriminator, ValidationError, RootModel, field_validator, Tag, create_model
 
-__all__ = [r'BaseTable', r'ProtocolTable', r'discriminator', r'MetaData', r'parse_meta', r'table_model']
+__all__ = [r'BaseTable', r'ProtocolMeta', r'discriminator', r'MetaData', r'parse_meta', r'table_model']
 
 
-class ProtocolTable(Protocol):
-    ИмяТаблицыХранения: str
+class ProtocolMeta(Protocol):
+
+    @property
+    def literal(self) -> str: ...
 
 
-# noinspection PyDataclass
-class BaseTable(BaseModel, frozen=True):
-    Назначение: Literal[r'Основная']
+class BaseMeta(BaseModel):
     Метаданные: PurePosixPath
-    ИмяТаблицы: str
-    ИмяТаблицыХранения: str
 
     # noinspection PyPep8Naming
     @field_validator(r'Метаданные', mode='before')
     def val_before_Метаданные(cls, v: str) -> str:
         return v.replace(r'.', r'/')
+
+
+class BaseField(BaseMeta):
+    ИмяПоля: str
+    ИмяПоляХранения: str
+
+    @property
+    def literal(self) -> str:
+        return self.ИмяПоляХранения
+
+
+class BaseTable(BaseMeta):
+    Назначение: Literal[r'Основная']
+    ИмяТаблицы: str
+    ИмяТаблицыХранения: str
+
+    Поля: dict[str, BaseField]  # = Field(validation_alias=AliasPath())
+
+    @property
+    def literal(self) -> str:
+        return self.ИмяТаблицыХранения
+
+    # noinspection PyPep8Naming
+    @field_validator(r'Поля', mode='before')
+    def val_before_Метаданные(cls, v: dict) -> dict[str, dict]:
+        return {v[r'ИмяПоля']: v for v in iter(v[r'Строки'])}
 
 
 def discriminator(v: dict) -> str:
@@ -40,7 +64,7 @@ def table_model(name: str, *, module: str, meta: str) -> Type[BaseTable]:
     return Annotated[create_model(name, __module__=module, __base__=BaseTable), Tag(meta)]
 
 
-def parse_meta[T=RootModel](meta: MetaData, *, descriptor: Type[T]) -> dict[str, T]:
+def parse_meta[T=RootModel](meta: MetaData, *, descriptor: Type[T]) -> dict[str, ProtocolMeta]:
     tables: list[T] = []
     for t in meta[r'Строки']:
         with suppress(ValidationError):
